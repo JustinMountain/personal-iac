891370562046c2cf60b56e234a8196ba3d7803c7
Justin Mountain <mountainj@gmail.com>
2024-08-19 21:30:55 -0400

    Terraform to Ubuntu VM

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# personal-iac
+# Personal Site Infrastructure as Code
+
+The repo has been initialized to create a bare-bones Ubunutu 24.04 server in Azure.
+
+As of 2024/08/19, a VM is created with a virtual NIC and security groups that allow SSH access.
--- main.tf
+++ main.tf
@@ -20,3 +20,109 @@ resource "azurerm_resource_group" "example-rg" {
   }
 }
 
+resource "azurerm_virtual_network" "example-vn" {
+  name                = "can-example-network"
+  resource_group_name = azurerm_resource_group.example-rg.name
+  location            = azurerm_resource_group.example-rg.location
+  address_space       = ["10.1.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+
+  # Set an explicit dependency
+  # depends_on = [azurerm_resource_group.example-rg]
+}
+
+resource "azurerm_subnet" "example-subnet" {
+  name                 = "can-example-subnet"
+  resource_group_name  = azurerm_resource_group.example-rg.name
+  virtual_network_name = azurerm_virtual_network.example-vn.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "example-sg" {
+  name                = "can-example-security-group"
+  location            = azurerm_resource_group.example-rg.location
+  resource_group_name = azurerm_resource_group.example-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "example-dev-rule" {
+  name                        = "can-example-dev-security-rule"
+  priority                    = 100
+  direction                   = "Inbound" # Azure uses deny by default
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"       # Restrict to IP
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example-rg.name
+  network_security_group_name = azurerm_network_security_group.example-sg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "example-sga" {
+  subnet_id                 = azurerm_subnet.example-subnet.id
+  network_security_group_id = azurerm_network_security_group.example-sg.id
+}
+
+resource "azurerm_public_ip" "example-ip" {
+  name                = "can-example-ip-1"
+  resource_group_name = azurerm_resource_group.example-rg.name
+  location            = azurerm_resource_group.example-rg.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_interface" "example-nic" {
+  name                = "can-example-nic-1"
+  resource_group_name = azurerm_resource_group.example-rg.name
+  location            = azurerm_resource_group.example-rg.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example-ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example-vm" {
+  name                = "can-example-vm-1"
+  resource_group_name = azurerm_resource_group.example-rg.name
+  location            = azurerm_resource_group.example-rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example-nic.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/exampleazurekey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
+
